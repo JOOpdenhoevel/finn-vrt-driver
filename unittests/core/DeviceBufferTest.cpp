56755978e25f955871a210ffb79c5d0deaed0a62
Jan-Oliver Opdenhövel <Jan-Oliver.Opdenhovel@amd.com>
2026-03-03 13:53:24 +0000

    Testing the full execution on the device buffer level
    Signed-off-by: Jan-Oliver Opdenhövel <Jan-Oliver.Opdenhovel@amd.com>

--- unittests/core/DeviceBufferTest.cpp
+++ unittests/core/DeviceBufferTest.cpp
@@ -30,11 +30,11 @@ using namespace FinnUnittest;
 class DBTest : public ::testing::Test {
      protected:
     vrt::Device device = vrt::Device("NULL", "../example_networks/single-layer-linear/finn_sim.vbin");
-    FinnUtils::BufferFiller filler = FinnUtils::BufferFiller(0, 255);
 
-    void SetUp() override {}
+    void SetUp() override { Logger::initLogger(true); }
     void TearDown() override { device.cleanup(); }
 };
+
 
 TEST_F(DBTest, DBStoreTest) {
     Finn::SyncDeviceInputBuffer<uint8_t> buffer("InputBuffer", device, FinnUnittest::myShapePacked, FinnUnittest::parts);
@@ -52,6 +52,22 @@ TEST_F(DBTest, DBOutputTest) {
     buffer.read();
     auto vec = buffer.getData(buffer.getTotalDataSize());
     EXPECT_EQ(data, vec);
+}
+
+TEST_F(DBTest, SyncExecutionTest) {
+    Finn::SyncDeviceInputBuffer<uint8_t> input_buffer("idma0", device, shape_t({1, 20, 4}), 1);
+    Finn::vector<uint8_t> input_data(input_buffer.getFeatureMapSize() * input_buffer.getBatchSize());
+    FinnUtils::BufferFiller(0, 255).fillRandom(input_data.begin(), input_data.end());
+    EXPECT_TRUE(input_buffer.store(std::span(input_data)));
+    EXPECT_TRUE(input_buffer.run());
+
+    Finn::SyncDeviceOutputBuffer<uint8_t> output_buffer("odma0", device, shape_t({1, 20, 4}), 1);
+    EXPECT_TRUE(output_buffer.run());
+    EXPECT_TRUE(output_buffer.wait());
+    EXPECT_TRUE(output_buffer.read());
+
+    Finn::vector<uint8_t> output_data = output_buffer.getData(input_buffer.getFeatureMapSize() * input_buffer.getBatchSize());
+    EXPECT_EQ(input_data, output_data);
 }
 
 int main(int argc, char** argv) {
