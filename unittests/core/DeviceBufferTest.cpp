3026c8ec35c367f799e19bcd4824bad2917c6185
Jan-Oliver Opdenhövel <Jan-Oliver.Opdenhovel@amd.com>
2026-04-07 11:23:03 +0100

    Testing memory interactions more cautiously
    Signed-off-by: Jan-Oliver Opdenhövel <Jan-Oliver.Opdenhovel@amd.com>

--- unittests/core/DeviceBufferTest.cpp
+++ unittests/core/DeviceBufferTest.cpp
@@ -28,7 +28,7 @@ using namespace FinnUnittest;
 
 class DBTest : public ::testing::Test {
      protected:
-    vrt::Device device = vrt::Device("0000:61:00", "../example_networks/single-layer-linear/finn_hw.vbin");
+    vrt::Device device = vrt::Device("0000:21:00", "../example_networks/identity_net/finn_hw.vbin");
 
     void SetUp() override {}
     void TearDown() override {}
@@ -75,31 +75,36 @@ TEST_F(DBTest, RawVRTExecutionTest) {
     vrt::Kernel idma0(device, "idma0");
     vrt::Kernel odma0(device, "odma0");
 
-    vrt::Buffer<uint8_t> input_buffer(device, 4096, vrt::MemoryRangeType::DDR);
-    vrt::Buffer<uint8_t> output_buffer(device, 4096, vrt::MemoryRangeType::DDR);
+    vrt::Buffer<uint8_t> input_buffer(device, 4096, idma0.portMemoryConfig("m_axi_gmem0"));
+    vrt::Buffer<uint8_t> output_buffer(device, 4096, odma0.portMemoryConfig("m_axi_gmem0"));
     std::cout << "Allocated buffers" << std::endl;
 
-    for (uint8_t i = 0; i < 10; i++) {
-        input_buffer[i] = i;
+    for (std::size_t i = 0; i < 20; i++) {
+        float input_value = static_cast<float>(i);
+        uint8_t* input_ptr = reinterpret_cast<uint8_t*>(&input_value);
+        for (std::size_t j = 0; j < sizeof(float); j++) {
+            input_buffer[i * sizeof(float) + j] = input_ptr[j];
+        }
     }
+
     input_buffer.sync(vrt::SyncType::HOST_TO_DEVICE);
     std::cout << "Synched data to the device" << std::endl;
 
-    idma0.start(input_buffer.getPhysAddr(), static_cast<uint32_t>(1));
-    odma0.start(output_buffer.getPhysAddr(), static_cast<uint32_t>(1));
+    idma0.start(input_buffer, static_cast<uint32_t>(1));
+    odma0.start(output_buffer, static_cast<uint32_t>(1));
     std::cout << "Started the kernels" << std::endl;
 
     idma0.wait();
     std::cout << "IDMA done" << std::endl;
     odma0.wait();
     std::cout << "ODMA done" << std::endl;
+
     output_buffer.sync(vrt::SyncType::DEVICE_TO_HOST);
     std::cout << "Results synched from the device" << std::endl;
 
-    for (std::size_t i = 1; i < 9; i++) {
-        EXPECT_EQ(output_buffer[0], output_buffer[i]);
+    for (std::size_t i = 1; i < 20; i++) {
+        EXPECT_EQ(output_buffer[0], i * 2);
     }
-    EXPECT_EQ(0, output_buffer[9]);
     std::cout << "Outputs verified, done!" << std::endl;
 }
 
